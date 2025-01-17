a21f5a848587fdd0b9d0fff07df0ba71440842cf
Laurent Constantin <laurent.constantin@liip.ch>
2025-01-17 11:22:02 +0100

    coverage indexer

--- tests/Indexer/IndexerTest.php
+++ tests/Indexer/IndexerTest.php
@@ -4,6 +4,10 @@ namespace Biblioteca\TypesenseBundle\Tests\Indexer;
 
 use Biblioteca\TypesenseBundle\Indexer\Indexer;
 use Biblioteca\TypesenseBundle\Indexer\IndexerInterface;
+use Biblioteca\TypesenseBundle\Mapper\DataGeneratorInterface;
+use Biblioteca\TypesenseBundle\Mapper\Entity\EntityTransformerInterface;
+use Biblioteca\TypesenseBundle\Mapper\Entity\Identifier\EntityIdentifierInterface;
+use Biblioteca\TypesenseBundle\Mapper\Locator\MapperLocatorInterface;
 use Biblioteca\TypesenseBundle\Populate\PopulateService;
 use Biblioteca\TypesenseBundle\Tests\Entity\Product;
 use Biblioteca\TypesenseBundle\Tests\KernelTestCase;
@@ -62,5 +66,77 @@ class IndexerTest extends KernelTestCase
         $indexer->removeEntity($product);
 
         $this->assertTrue(true); // @phpstan-ignore-line Mock handle the test.
+    }
+
+    public function testNoIndexOnUnsupportedEntity(): void
+    {
+        // Mock the PopulateService to assert 'fillData' is called once
+        $populateMock = $this->createMock(PopulateService::class);
+        $populateMock->expects($this->never())
+            ->method('fillData');
+
+        $mapperLocator = new class implements MapperLocatorInterface {
+            public function hasDataGenerator(string $name): bool
+            {
+                return true;
+            }
+
+            public function getMappers(): array
+            {
+                return [];
+            }
+
+            public function countDataGenerator(): int
+            {
+                return 0;
+            }
+
+            /**
+             * @template T of object
+             *
+             * @param class-string<T> $entity
+             *
+             * @return EntityTransformerInterface<T>[]
+             */
+            public function getEntityTransformers(string $entity): array
+            {
+                $myEntityTransformer = new class implements EntityTransformerInterface {
+                    public function support(object $entity): bool
+                    {
+                        return false;
+                    }
+
+                    public function transform(object $entity): array
+                    {
+                        return [];
+                    }
+                };
+
+                /** @var array<string, EntityTransformerInterface<T>> $result */
+                $result = ['myEntityTransformer' => $myEntityTransformer];
+
+                return $result;
+            }
+
+            public function hasEntityTransformer(string $classString): bool
+            {
+                return true;
+            }
+
+            public function getDataGenerator(string $shortName): DataGeneratorInterface
+            {
+                throw new \RuntimeException('Not implemented');
+            }
+        };
+
+        $entityIdentifier = new class implements EntityIdentifierInterface {
+            public function getIdentifiersValue(object $entity): array
+            {
+                return ['id' => 'fake'];
+            }
+        };
+        $indexer = new Indexer($populateMock, $mapperLocator, $entityIdentifier);
+        $indexer->indexEntity(new Product());
+        $indexer->removeEntity(new Product());
     }
 }
